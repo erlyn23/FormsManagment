199b44e76c6e52e5ec9268c319b4dcc43c640a76
erlyn23 <erlynetv@gmail.com>
2021-07-08 16:08:21 -0400

    Se guardan forms y preguntas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from '@shared/guards/auth.guard';
-import { LoginComponent } from './componnets/auth/login/login.component';
-import { RegisterComponent } from './componnets/auth/register/register.component';
-import { DashboardComponent } from './componnets/home/dashboard/dashboard.component';
-import { QuestionsComponent } from './componnets/home/questions/questions.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { DashboardComponent } from './components/home/dashboard/dashboard.component';
+import { QuestionsComponent } from './components/home/questions/questions.component';
 
 const routes: Routes = [{path: '', pathMatch: 'full', component: LoginComponent},
 {path: 'login', component: LoginComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './componnets/auth/login/login.component';
-import { RegisterComponent } from './componnets/auth/register/register.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { MaterialModule } from '@shared/material/material.module';
@@ -12,10 +12,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from '@shared/header/header.component';
 import { AddDialogComponent } from '@shared/components/add-dialog/add-dialog.component';
 import { ConfirmDialogComponent } from './shared/components/confirm-dialog/confirm-dialog.component';
-import { DashboardComponent } from './componnets/home/dashboard/dashboard.component';
-import { QuestionsComponent } from './componnets/home/questions/questions.component';
+import { DashboardComponent } from './components/home/dashboard/dashboard.component';
+import { QuestionsComponent } from './components/home/questions/questions.component';
 import { QuestionFormDialogComponent } from '@shared/components/question-form-dialog/question-form-dialog.component';
-import { QuestionOptionsComponent } from './componnets/home/questions/question-options/question-options.component';
+import { QuestionOptionsComponent } from './components/home/questions/question-options/question-options.component';
 
 @NgModule({
   declarations: [
